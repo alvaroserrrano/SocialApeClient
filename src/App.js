fb79c509a589b05ac8dc63391ede2bbe3068290b
Alvaro <alvaro.serrano@wmich.edu>
2019-07-10 19:41:00 +0200

    login

--- src/App.js
+++ src/App.js
@@ -14,45 +14,74 @@ import login from './pages/login';
 import signup from './pages/signup';
 
 const theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: '#484848',
-			main: '#212121',
-			dark: '#000000',
-			contrastText: '#fff'
-		},
-		secondary: {
-			light: '#b0ff57',
-			main: '#76ff03',
-			dark: '#32cb00',
-			contrastText: '#000'
-		},
-		type: 'dark',
-		typography: {
-			useNextVariants: true
-		}
-	}
+  palette: {
+    primary: {
+      light: '#484848',
+      main: '#212121',
+      dark: '#000000',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#b0ff57',
+      main: '#76ff03',
+      dark: '#32cb00',
+      contrastText: '#000'
+    },
+    type: 'dark',
+    typography: {
+      useNextVariants: true
+    },
+    form: {
+      textAlign: 'center'
+    },
+    image: {
+      margin: '20px auto 20px'
+    },
+    pageTitle: {
+      margin: '10px auto 10px'
+    },
+    textField: {
+      margin: '10px auto 10px'
+    },
+    button: {
+      marginTop: '20px',
+      marginBottom: '20px',
+      position: 'relative'
+    },
+    customError: {
+      color: 'red',
+      fontSize: '0.8 rem',
+      marginTop: '10px'
+    },
+    smallText: {
+      marginTop: '25px',
+      color: '#fff'
+    },
+    progress: {
+      position: 'absolute'
+    }
+  }
 });
 
 class App extends Component {
-	render() {
-		return (
-			<MuiThemeProvider theme={theme}>
-				<div className='App'>
-					<Router>
-						<Navbar />
-						<div className='container'>
-							<Switch>
-								<Route exact path='/' component={home} />
-								<Route exact path='/login' component={login} />
-								<Route exact path='/signup' component={signup} />
-							</Switch>
-						</div>
-					</Router>
-				</div>
-			</MuiThemeProvider>
-		);
-	}
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className='App'>
+          <Router>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={home} />
+                <Route exact path='/login' component={login} />
+                <Route exact path='/signup' component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
