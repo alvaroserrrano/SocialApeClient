fc8c81e967cbce1354e4f0f33bcee1f97be0288a
Alvaro <alvaro.serrano@wmich.edu>
2019-07-11 23:30:49 +0200

    signup action started

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,7 +9,9 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+//Redux
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
 const styles = {
   form: {
     textAlign: 'center'
@@ -53,20 +55,29 @@ class login extends Component {
   }
   handleSubmit = event => {
     event.preventDefault();
-
     const userData = {
       email: this.state.email,
       password: this.state.password
     };
+    this.props.loginUser(userData, this.props.history);
   };
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({ errors: nextProps.UI.errors });
+    }
+  }
+
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
   };
   render() {
-    const { classes } = this.props;
-    const { loading, errors } = this.state;
+    const {
+      classes,
+      UI: { loading }
+    } = this.props;
+    const { errors } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
@@ -141,7 +152,22 @@ class login extends Component {
 }
 
 login.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  loginUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(login);
+const mapStateToProps = state => ({
+  user: state.user,
+  UI: state.UI
+});
+
+const mapActionsToProps = {
+  loginUser
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(login));
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -21,6 +21,26 @@ export const loginUser = (userData, history) => dispatch => {
     });
 };
 
+export const signupUser = (newUserData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post('/signup', newUserData)
+    .then(res => {
+      const FBIdToken = `Bearer ${res.data.token}`;
+      localStorage.setItem('FBIdToken', FBIdToken);
+      axios.defaults.headers.common['Authorization'] = FBIdToken;
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push('/');
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
 export const getUserData = () => dispatch => {
   axios
     .get('/user')
