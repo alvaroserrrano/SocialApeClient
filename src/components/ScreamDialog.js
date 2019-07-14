9f6d5e66dbe255fcaab841982744c6868c51ff8e
Alvaro <alvaro.serrano@wmich.edu>
2019-07-14 23:42:34 +0200

    max depth exceeded

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -17,8 +17,36 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 //Icons
 import CloseIcon from '@material-ui/icons/Close';
+import UnfoldMore from '@material-ui/icons/UnfoldMore';
 
-const styles = {};
+const styles = () => ({
+  invisibleSeparator: {
+    border: 'none',
+    margin: 4
+  },
+  profileImage: {
+    maxWidth: 200,
+    height: 200,
+    borderRadius: '50%',
+    objectFit: 'cover'
+  },
+  dialogContent: {
+    padding: 20
+  },
+  closeButton: {
+    position: 'absolute',
+    left: '90%'
+  },
+  expandButton: {
+    position: 'absolute',
+    left: '90%'
+  },
+  spinnerDiv: {
+    textAlign: 'center',
+    marginTop: 50,
+    marginBottom: 50
+  }
+});
 
 class ScreamDialog extends Component {
   state = {
@@ -35,12 +63,74 @@ class ScreamDialog extends Component {
       open: false
     });
   };
-    render() {
-        const {classes, scream: {screamId, body, createdAt, likeCount, commentCount, userImage, userHandle}}
-        return (
-
-        )
-    }
+  render() {
+    const {
+      classes,
+      scream: {
+        screamId,
+        body,
+        createdAt,
+        likeCount,
+        commentCount,
+        userImage,
+        userHandle
+      },
+      UI: { loading }
+    } = this.props;
+    const dialogMarkup = loading ? (
+      <CircularProgress className={classes.spinnerDiv} size={200} />
+    ) : (
+      <Grid container spacing={16}>
+        <Grid item sm={5}>
+          <img src={userImage} alt='Profile' className={classes.profileImage} />
+        </Grid>
+        <Grid item sm={7}>
+          <Typography
+            component={Link}
+            color='primary'
+            variant='h5'
+            to={`/users/${userHandle}`}
+          >
+            {userHandle}
+          </Typography>
+          <hr className={classes.invisibleSeparator} />
+          <Typography variant='body2' color='textSecondary'>
+            {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+          </Typography>
+          <hr className='invisibleSeparator' />
+          <Typography variant='body1'>{body}</Typography>
+        </Grid>
+      </Grid>
+    );
+    return (
+      <Fragment>
+        <MyButton
+          onClick={this.handleOpen()}
+          tip='Expand scream'
+          tipClassName={classes.expandButton}
+        >
+          <UnfoldMore color='primary' />
+        </MyButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth='sm'
+        >
+          <MyButton
+            tip='Close'
+            onClick={this.handleClose}
+            tipClassName={classes.closeButton}
+          >
+            <CloseIcon />
+          </MyButton>
+          <DialogContent className={classes.dialogContent}>
+            {dialogMarkup}
+          </DialogContent>
+        </Dialog>
+      </Fragment>
+    );
+  }
 }
 
 ScreamDialog.propTypes = {
@@ -58,5 +148,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  mapActionsToProps
+  { getScream }
 )(withStyles(styles)(ScreamDialog));
